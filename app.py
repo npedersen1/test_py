d09d54dca3ab8b4da989268f6d7b4cb7e4c7cf65
Nathan <ntpedersen1@gmail.com>
2018-08-09 00:03:02 -0800

    Edited app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-str = "This is a python program"
-print(str)
+from math import *
 
-array = ["Supa", "Hot", "Fire"]
-for elements in array:
-    print(elements)
+stren_lvl = 99
+pot_bonus = 3 + floor(99 * .1)
+pray_bonus = 1.23
+other_bonus = 7/6
+style_bonus = 3
+str_bonus = 143
+
+eff_stren = floor((stren_lvl + pot_bonus) * pray_bonus * other_bonus) + style_bonus
+base_dam = 1.3 + (eff_stren / 10) + (str_bonus / 80) + ((eff_stren * str_bonus) / 640)
+
+max_hit = floor(base_dam)
+
+print(max_hit)
